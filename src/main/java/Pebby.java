d6fc655e1fb4e4de02131176a81fd42956c56dd9
0218hy <leehayoung0218@gmail.com>
2026-08-20 14:21:19 +0800

    Add greetings

--- src/main/java/Pebby.java
+++ src/main/java/Pebby.java
@@ -6,6 +6,12 @@ public class Pebby {
                 + "|  __/|  __/ |_) | |_) | |_| |\n"
                 + "|_|    \\___|_.__/|_.__/ \\__, |\n"
                 + "                         |___/\n";
-        System.out.println(banner);
+        System.out.println("____________________________________________________________");
+        System.out.print(banner);
+        System.out.println("Hello! I'm Pebby.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye Bye!");
+        System.out.println("____________________________________________________________");
     }
 }
