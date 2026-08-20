e85c49e3af5cfb30b858824a648e9b2e0b73c5e4
0218hy <leehayoung0218@gmail.com>
2026-08-20 14:36:46 +0800

    Add support for echo

--- src/main/java/Pebby.java
+++ src/main/java/Pebby.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Pebby {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String banner = " ____       _     _          \n"
                 + "|  _ \\  ___| |__ | |__  _   _\n"
                 + "| |_) |/ _ \\ '_ \\| '_ \\| | | |\n"
@@ -10,6 +14,16 @@ public class Pebby {
         System.out.print(banner);
         System.out.println("Hello! I'm Pebby.");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // Getting user input
+        String command = scanner.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(command);
+            System.out.println("____________________________________________________________");
+            command = scanner.nextLine();
+        }
         System.out.println("____________________________________________________________");
         System.out.println("Bye Bye!");
         System.out.println("____________________________________________________________");
