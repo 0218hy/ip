6ddc2d5e5871ed36a1189ae02cc2d56e4d68dcf6
0218hy <leehayoung0218@gmail.com>
2026-08-28 01:15:53 +0800

    Add Javadoc comments

--- src/main/java/pebby/task/Event.java
+++ src/main/java/pebby/task/Event.java
@@ -4,11 +4,14 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 /** Represents a task that takes place between a supplied start and end time. */
-public class Event extends Task{
+public class Event extends Task {
+    /** The date on which this event begins. */
     protected LocalDate from;
+    /** The date on which this event ends. */
     protected LocalDate to;
 
-    public Event(String description, String from, String to){
+    /** Creates an event using start and end dates in {@code MMM dd yyyy} format. */
+    public Event(String description, String from, String to) {
         super(description);
         this.from = LocalDate.parse(from, DateTimeFormatter.ofPattern("MMM dd yyyy"));
         this.to = LocalDate.parse(to, DateTimeFormatter.ofPattern("MMM dd yyyy"));
@@ -24,6 +27,7 @@ public class Event extends Task{
         return to.toString();
     }
 
+    /** Returns the user-facing representation of this event. */
     @Override
     public String toString() {
         return "[E] " + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
