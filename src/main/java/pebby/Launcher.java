49784b7db25af3f7fa02e72530eddbee41ac0112
0218hy <leehayoung0218@gmail.com>
2026-09-03 17:05:15 +0800

    Recreate JaxaFX Tutorial-Part1

---
+++ src/main/java/pebby/Launcher.java
@@ -0,0 +1,18 @@
+package pebby;
+
+import javafx.application.Application;
+
+/**
+ * Launches Pebby's JavaFX application without JavaFX classpath issues.
+ */
+public class Launcher {
+
+    /**
+     * Launches the JavaFX application.
+     *
+     * @param args command-line arguments passed to the application
+     */
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
