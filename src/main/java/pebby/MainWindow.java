badb054acc28e1603377d752b0f76605c5594bb2
0218hy <leehayoung0218@gmail.com>
2026-09-15 13:00:51 +0800

    Fix CI issue regarding Checkstyle failure

--- src/main/java/pebby/MainWindow.java
+++ src/main/java/pebby/MainWindow.java
@@ -8,7 +8,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-
 import pebby.parser.CommandType;
 import pebby.parser.ParsedCommand;
 import pebby.parser.Parser;
